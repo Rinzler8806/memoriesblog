e69315ba99d1c9d1161ad5d7ca02b59ad7ae347c
Rinzler8806 <shane.rogers916@gmail.com>
2021-05-29 19:28:43 -0700

    redux added

--- server/index.js
+++ server/index.js
@@ -7,11 +7,12 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
+
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://Rinzler8806:Darktide88@cluster0.kwo9h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
