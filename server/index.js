daf0786b57c89125fc3b31a0d5339734348b2a3a
Rinzler8806 <shane.rogers916@gmail.com>
2021-06-24 22:46:55 -0700

    json web token backend and authentication created.

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
 dotenv.config();
@@ -13,7 +15,9 @@ dotenv.config();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
 app.get('/', (req, res) => {
     res.send('Accessed memories API')
